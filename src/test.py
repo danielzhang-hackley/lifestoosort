23b211063a2571d0b299898871671ca93288fef4
Daniel F. Zhang <danielfzhang@gmail.com>
2022-11-26 21:00:23 -0500

    working on edge cases; saved to 'satisfies_edge_cases' branch

--- src/test.py
+++ src/test.py
@@ -1,1 +1,23 @@
-print("hello world")
+import utils
+import numpy as np
+
+print('\033c')
+
+"""
+[[[383 163]]
+
+ [[382 164]]
+
+ [[374 164]]
+
+ [[373 165]]]
+
+"""
+
+x = np.array([1, 2, 3])
+y = np.array([[1],
+              [2],
+              [3]])
+
+print(utils.add_affine(x, axis=1))
+print(utils.add_affine(y, axis=1))
