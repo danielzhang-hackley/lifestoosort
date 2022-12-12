6cd2fbc275bdf136494b1ed0d340bc5c21705ef1
danielzhang-hackley <77632294+danielzhang-hackley@users.noreply.github.com>
2022-12-11 21:57:44 -0600

    Add files via upload

---
+++ neopixel-ring-engi120.ino
@@ -0,0 +1,35 @@
+// NeoPixel Ring simple sketch (c) 2013 Shae Erisson
+// Released under the GPLv3 license to match the rest of the
+// Adafruit NeoPixel library
+
+
+// number for 0 to 255 representing how bright the LEDs will be
+#define BRIGHTNESS  100
+
+
+
+
+
+#include <Adafruit_NeoPixel.h>
+
+// Which pin on the Arduino is connected to the NeoPixels?
+#define PIN        6 // On Trinket or Gemma, suggest changing this to 1
+
+// How many NeoPixels are attached to the Arduino?
+#define NUMPIXELS  24
+
+// When setting up the NeoPixel library, we tell it how many pixels,
+// and which pin to use to send signals. Note that for older NeoPixel
+// strips you might need to change the third parameter -- see the
+// strandtest example for more information on possible values.
+Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+void setup() {
+  pixels.begin();
+
+  pixels.fill(pixels.Color(0, 0, BRIGHTNESS));
+  pixels.show();
+}
+
+void loop() {
+}
